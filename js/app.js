d957bd10bc011870ac94f0f9d94f65c25fdbf953
Sarah Wood <wood.sarah@outlook.com>
2020-11-20 13:47:23 +0000

    Options randomiser added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+document.getElementById('decider-btn').addEventListener('click', function() {
+  document.getElementById('result').textContent = randomTakeaway;
+});
+
+
+const takeawayOptions = ['Burgers', 'Fish and Chips', 'Fried Chicken', 'Grilled Chicken', 'Kebab', 'Curry', 'Sushi', 'Pasta', 'Pizza', 'Salad'];
+
+// takeaway randomiser
+const randomTakeaway = () => {
+  return Math.floor(Math.random(takeawayOptions)) * 10;
+}
